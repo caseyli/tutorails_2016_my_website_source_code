9fa572b097883dd9287ea266165eb15a97924997
Casey Li <casey.li@gmail.com>
2016-03-23 00:34:38 +0000

    Added new and create actions.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -13,4 +13,21 @@ class BlogPostsController < ApplicationController
     render("show")
   end
 
+  def new
+    @blog_post = BlogPost.new
+    render("new")
+  end
+
+  def create
+    blog_post_params = params.require(:blog_post).permit(:title, :body)
+    @blog_post = BlogPost.new(blog_post_params)
+
+    if @blog_post.save
+      redirect_to "/blog_posts/" + @blog_post.id.to_s
+    else
+      render("new")
+    end
+
+  end
+
 end
